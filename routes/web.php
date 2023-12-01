5a769400c675808505b188e8e0de8beaa9bd7945
Muhammad Iqbal Gobel <muhammadiqbal.g@yahoo.com>
2023-12-01 16:29:05 +0700

    adding env

--- routes/web.php
+++ routes/web.php
@@ -41,24 +41,26 @@ Route::get('/email/verify', function () {
 //The Email Verification Handler
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-    
+
     return redirect('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 //Resending The Verification Email
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
-    
+
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-// Route::get('/dashboard', function () {
-//     return view('admin.dashboard-admin');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+$adminMiddleware = ['auth','verified', 'role:admin'];
+$userMiddleware = ['auth', 'verified'];
 
-Route::get('/dashboard', [DashboardController::class, 'checkAuthentication'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/home', [HomeController::class, 'HomeUser'])->middleware(['auth', 'verified'])->name('home');
-Route::post('/home', [HomeController::class, 'addTestimoni'])->middleware(['auth', 'verified'])->name('testimoni.store');
+//AdminRoleMiddleware
+Route::middleware($adminMiddleware)->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard-admin');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+});
 
 Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
 Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('google.callback');
@@ -84,6 +86,16 @@ Route::get('/orderSablon/Detail', [OrderController::class, 'index_detailOrder_us
 // Route::get('/orderSablon', [SablonController::class, 'OrderSablon'])->middleware(['auth', 'verified'])->name('orderSablon');
 Route::get('/tracking', [TrackingController::class, 'tracking'])->middleware(['auth', 'verified'])->name('tracking');
 Route::get('/testimoni', [TestimoniController::class, 'index'])->middleware(['auth', 'verified'])->name('testimoni.index');
+
+Route::middleware($userMiddleware)->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'checkAuthentication'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/home', [HomeController::class, 'HomeUser'])->middleware(['auth', 'verified'])->name('home');
+    Route::post('/home', [HomeController::class, 'addTestimoni'])->middleware(['auth', 'verified'])->name('testimoni.store');
+
+
+
+
+});
 
 
 require __DIR__.'/auth.php';
